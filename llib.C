013f149ef2def4d7e526f75e6487332f514d95b8
p3tru <stephane.zsoldos@gmail.com>
2019-06-17 22:36:56 +0100

    Fix llib.C

--- llib.C
+++ llib.C
@@ -6,7 +6,7 @@ void llib(){
         if (wcsimdirenv != NULL) {
                 gSystem->Load("$WCSIMDIR/libWCSimRoot.so");
                 gSystem->Load("$WCSIMDIR/libWCSimRoot.rootmap");
-                gSystem->Load("$WCSIMDIR/libWCSimRootDict_rdict.pcm");
+                gSystem->Load("$WCSIMDIR/src/WCSimRootDict_rdict.pcm");
         } else {
                 std::cout << "Error: WCSIMDIR environment variable not set." << std::endl;
         }
